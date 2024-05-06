3971fef463970191bd65fbd9e37d12444910f5d3
AdvaithRavishankar <83779786+AdvaithRavishankar@users.noreply.github.com>
2024-05-06 16:02:16 -0700

    Added voice list updating

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,41 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
+
+
+
 function init() {
-  // TODO
+  // adding voice list
+  function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+      return;
+    }
+  
+    const voices = speechSynthesis.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+
+  
 }
